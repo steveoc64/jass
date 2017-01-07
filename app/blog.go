f98fdf74c202a3eb11f8af27ed288b1a603b7f89
SteveOC64 <steveoc64@gmail.com>
2017-01-07 23:26:34 +1030

    split social buttons into its own file

--- app/blog.go
+++ app/blog.go
@@ -7,7 +7,6 @@ import (
 	"./shared"
 
 	"github.com/go-humble/router"
-	"github.com/gopherjs/gopherjs/js"
 	"honnef.co/go/js/dom"
 )
 
@@ -243,7 +242,6 @@ func showBlogItem(context *router.Context) {
 	w.ScrollTo(0, 0)
 	fadeIn("jass-blog-article")
 	noButtons()
-	print("this")
 
 	doc.QuerySelector(".jass-blog-article").AddEventListener("click", false, func(evt dom.Event) {
 		evt.PreventDefault()
@@ -257,67 +255,16 @@ func showBlogItem(context *router.Context) {
 		}
 	})
 
-	print("that")
-
 	if Session.ScrollFunc != nil {
 		w.RemoveEventListener("scroll", false, Session.ScrollFunc)
 		Session.ScrollFunc = nil
 	}
 
-	print("maybe")
 	doc.QuerySelector(".blog-article").AddEventListener("scroll", false, blogArticleScroller)
 	articleState = 0
 
 	// Add social buttons
-	print("here")
 	addSocialButtons(theBlog.GetURL(), theBlog.Name)
-}
-
-func addSocialButtons(url, name string) {
-	w := dom.GetWindow()
-	doc := w.Document()
-
-	print("adding social buttons")
-
-	// Twitworld
-	doc.QuerySelector(".share-twitter").AddEventListener("click", false, func(evt dom.Event) {
-		print("clicked on the twitworld thing")
-		w.Open(fmt.Sprintf(`%s?text=%s %s`,
-			"https://twitter.com/intent/tweet",
-			url,
-			name),
-			"twitter",
-			"menubar=0,resizable=1,width=400,height=280")
-	})
-
-	// Faceworld
-	doc.QuerySelector(".share-facebook").AddEventListener("click", false, func(evt dom.Event) {
-		print("clicked on the faceworld thing")
-		FB := js.Global.Get("FB")
-		print("fb", FB)
-		jQuery("loginbutton,#feedbutton").RemoveAttr("disabled")
-		FB.Call("getLoginStatus", func(r interface{}) {
-			print("in update status callback", r)
-		})
-		FB.Call("ui", js.M{
-			"method": "share",
-			"href":   url,
-			"quote":  fmt.Sprintf("Chamelee Blog - %s\n%s", name, url),
-		}, func(r interface{}) {
-			print("completed UI call", r)
-		})
-	})
-
-	// GWorld
-	doc.QuerySelector(".share-google").AddEventListener("click", false, func(evt dom.Event) {
-		print("clicked on the googleworld thing")
-		w.Open("https://plus.google.com/share?url="+url,
-			"gplus",
-			"menubar=no,toolbar=no,resizable=yes,scrollbar=yes,width=600,height=480")
-		// gapi := js.Global.Get("gapi")
-		// print("google api is", gapi)
-		// gapi.Get("plusone").Call("go")
-	})
 }
 
 var articleState = 0
